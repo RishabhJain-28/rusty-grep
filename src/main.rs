2f3bd2a8e4532baeaff97048d90eb13422025d30
Rishabh Jain <rishabhnjain@gmail.com>
2022-11-30 01:52:32 +0530

    refactor and error handling

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,20 @@
+use rusty_grep::Config;
 use std::env;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
 
-    let search_query = &args[1];
-    let file_path = &args[2];
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1)
+    });
 
-    println!("Searching for {}", search_query);
-    println!("In file {}", file_path);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    if let Err(e) = rusty_grep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
+    };
 }
