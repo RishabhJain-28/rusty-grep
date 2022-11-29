a87f96e654b97e1df2ce817989a29c53006f7d24
Rishabh Jain <rishabhnjain@gmail.com>
2022-11-30 02:42:34 +0530

    Std error and Added readme

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ fn main() {
     // dbg!(args);
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
 
     // println!("Searching for {}", config.query);
     // println!("In file {}", config.file_path);
     if let Err(e) = rusty_grep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
